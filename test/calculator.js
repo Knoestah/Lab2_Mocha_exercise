d88f59917b0e2af8491fe111253ece514e4e1d8f
Frederic Knoester <free.knoester@gmail.com>
2022-01-30 11:09:09 -0500

    Added lab exercise

--- test/calculator.js
+++ test/calculator.js
@@ -2,33 +2,28 @@ var assert = require('assert')
 
 const calculator = require('../app/calculator')
 
-describe('Calculator Test Cases', function () {
-    // Add tests
+describe('Calculator Test Cases', function () {    
     it('add(5,2) expected result 7', function () {
         assert.equal(calculator.add(5,2), 7)
     })
     it('add(5,2) expected result 9', function () {
         assert.equal(calculator.add(5,2), 9)
     })
-
-
-    // Sub tests
+   
     it('sub(5,2) expected result 3', function () {
         assert.equal(calculator.sub(5,2), 3)
     })
     it('sub(5,2) expected result 7', function () {
         assert.equal(calculator.sub(5,2), 7);
     })
-
-    // Mul tests
+    
     it('mul(5,2) expected result 10', function () {
         assert.equal(calculator.mul(5,2), 10)
     })
     it('mul(5,2) expected result 14', function () {
         assert.equal(calculator.mul(5,2), 14)
     })
-
-    // Div tests
+    
     it('div(10,2) expected result 5', function () {
         assert.equal(calculator.div(10,2), 5)
     })
